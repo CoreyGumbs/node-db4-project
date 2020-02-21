2a9c14f45cf7c9e1e5ec49170e385a5558ad071f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-20 21:59:57 -0500

    [Add] Added table migrations, all tables created in db

--- data/migrations/20200220212253_create_recipe_tables.js
+++ data/migrations/20200220212253_create_recipe_tables.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('recipe_steps', tbl => {
+        tbl.increments(); 
+        tbl.string('instruction', 255).notNullable();
+
+          //foreign key to recipe
+          tbl.integer('recipe_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('recipe')  
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+    })
+    .createTable('recipe', tbl => {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('recipe')
+    .dropTableIfExists('recipe_steps')
 };
