cf817f3f74e59bbd3060902e19c97fc4556d0702
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-20 23:56:19 -0500

    [Add] Added all recipe CRUD enpoints and tested with postman. working

--- router/router.js
+++ router/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../data/db-config');
 const router = express.Router();
 
-
+//Recipes
 router.get('/', (req, res) => {
     db('recipe')
     .then(recipe => {
@@ -23,5 +23,70 @@ router.get('/:id', (req, res) => {
         res.status(500).json({error: 'There was an error retrieving recipe'});
     });
 });
+
+router.post('/', (req, res) => {
+    db('recipe')
+    .insert(req.body)
+    .then(recipe => {
+        res.status(200).json(recipe);
+    })
+    .catch(error => {
+        res.status(500).json({error: 'There was an error adding recipe'});
+    })
+});
+
+router.put('/:id', (req, res) => {
+    db('recipe')
+    .update(req.body)
+    .where({id: req.params.id})
+    .then(recipe => {
+        res.status(200).json(recipe);
+    })
+    .catch(error => {
+        res.status(500).json({error: 'There was an error updating recipe'});
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    db('recipe')
+    .where({id: req.params.id})
+    .del()
+    .then(recipe => {
+        res.status(200).json(recipe);
+    })
+    .catch(error => {
+        res.status(500).json({error: 'There was an error deleting recipe'});
+    })
+})
+
+//Recipe Steps
+
+//Ingredients
+
+router.get('/ingredients', (req, res) => {
+    db('ingredients')
+    .then(ingredients => {
+        console.log(ingredients);
+        res.status(200).json(ingredients);
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was an issue retrieving the ingredients"});
+    })
+})
+
+router.get('/ingredients/:id', (req, res) => {
+    db('ingredients')
+    .where({id: req.params.id})
+    .first()
+    .then(recipe => {
+        res.status(200).json(recipe);
+    })
+    .catch(error => {
+        res.status(500).json({error: 'There was an error retrieving recipe'});
+    });
+});
+
+
+
 
 module.exports = router;
